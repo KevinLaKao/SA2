42e9eb25a8394a3f92eb8fd8c3fabe835eb65109
林承毅 <iven920703@gmail.com>
2023-05-16 17:33:04 +0800

    userPhotoChange

--- function/addProductToCart.php
+++ function/addProductToCart.php
@@ -5,14 +5,14 @@ if (!empty($_GET['userEmail'])) {
     $userEmail = $_GET['userEmail'];
 } else {
 ?>
-    <script>
-        alert(" 加入購物車失敗請先登入 ！");
-        location = '../productSeller.php';
-    </script>
-    <?php
+<script>
+alert(" 加入購物車失敗請先登入 ！");
+location = '../productSeller.php';
+</script>
+<?php
 }
 
-$link = mysqli_connect('localhost', 'root', '', 'sa');
+$link = mysqli_connect('localhost', 'root', '12345678', 'sa');
 $isExistSql = "select * from cart";
 $result = mysqli_query($link, $isExistSql);
 while ($row = mysqli_fetch_array($result)) {
@@ -22,34 +22,34 @@ while ($row = mysqli_fetch_array($result)) {
             where productCode='$productCode'";
         if (mysqli_query($link, $update)) {
     ?>
-            <script>
-                alert(" 更改商品成功 ！");
-                location = '../productSeller.php';
-            </script>
+<script>
+alert(" 更改商品成功 ！");
+location = '../productSeller.php';
+</script>
 <?php
         }
     }
 }
 ?>
 <script>
-    alert(" 正在新增 ！");
+alert(" 正在新增 ！");
 </script>
 <?php
 $sql  = "insert into cart (productCode, userEmail) 
             values ('$productCode', '$userEmail')";
 if (mysqli_query($link, $sql)) {
 ?>
-    <script>
-        alert(" 新 增 成 功 ！");
-        location = '../productSeller.php';
-    </script>
+<script>
+alert(" 新 增 成 功 ！");
+location = '../productSeller.php';
+</script>
 <?php
 } else {
 ?>
-    <script>
-        alert(" 新 增 失 敗 ！");
-        location = '../productSeller.php';
-    </script>
+<script>
+alert(" 新 增 失 敗 ！");
+location = '../productSeller.php';
+</script>
 <?php
 }
 ?>
